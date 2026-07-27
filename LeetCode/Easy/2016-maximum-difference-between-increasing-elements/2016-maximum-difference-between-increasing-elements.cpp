393baef0a7eb534a3ba3154ec327110ea813c3b2
ILESH KUMAR <ilesh.25bai10018@vitbhopal.ac.in>
2026-07-27 12:06:45 +0530

    2016-maximum-difference-between-increasing-elements 0 ms (100%) 12.1 MB (8.42%)

---
+++ LeetCode/Easy/2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int maxdiff = -1;
+        int minval = nums[0];
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > minval) {
+                maxdiff = max(maxdiff, nums[i] - minval);
+            } else {
+                minval = nums[i];
+            }
+        }
+        
+        return maxdiff;
+    }
+};
