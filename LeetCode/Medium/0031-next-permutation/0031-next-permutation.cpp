960807bfd390fba5de17b09db60ee5ee70bb19ea
ILESH KUMAR <ilesh.25bai10018@vitbhopal.ac.in>
2026-08-18 02:45:06 +0530

    0031-next-permutation 0 ms (100%) 15.8 MB (53.09%)

---
+++ LeetCode/Medium/0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& A) {
+        int n = A.size();
+        int pivot = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (A[i] < A[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+        if (pivot != -1) {
+            for (int i = n - 1; i > pivot; i--) {
+                if (A[i] > A[pivot]) {
+                    swap(A[i], A[pivot]);
+                    break;
+                }
+            }
+        }
+        reverse(A.begin() + pivot + 1, A.end());
+    }
+};
